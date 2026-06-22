c1344a23f35e5e11b86e0ac852de6438ce2561a1
Maryce Souzino <malutecinf@hotmail.com>
2026-06-22 20:34:58 -0300

    fix: conectar MongoDB antes de handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 const app = require('../server');
+const mongoose = require('mongoose');
 
-module.exports = app;
+module.exports = async function handler(req, res) {
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(process.env.MONGODB_URI);
+  }
+  return app(req, res);
+};
